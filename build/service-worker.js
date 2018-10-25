751ad1d2566f6d3c3bb5468731dfa388e74b85a1
ARahman <a.rahman1783@yahoo.com>
2018-10-25 16:14:48 +0200

    Modify date

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
-importScripts("precache-manifest.d4291a855e411f80feac307e046dbb15.js");
+importScripts(
+  "/arahman1983/expensefy-app/precache-manifest.e7a613dbaaa74e18f7d0502f2fc743fe.js"
+);
 
 workbox.clientsClaim();
 
@@ -28,6 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
-  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
+workbox.routing.registerNavigationRoute("/arahman1983/expensefy-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/component/expenseItems.js
+++ src/component/expenseItems.js
@@ -2,12 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import { removeExpens } from "../actions/expenses";
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 const ExpenseItemList = ({ dispatch, id, description, amount, createdAt }) => (
   <tr>
     <td className="col">{description}</td>
     <td className="col">{amount}</td>
-    <td className="col">{createdAt}</td>
+    <td className="col">{moment(createdAt).format("MMM Do, YYYY")}</td>
     <td className="col">
       <Link className="btn btn-info" to={`/edit/${id}`}>
         Edit
