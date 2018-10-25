eb2458a265f48f0f10ef17a36cadee58b44d159e
ARahman <a.rahman1783@yahoo.com>
2018-10-25 16:24:31 +0200

    Editing paths in build

--- build/precache-manifest.e7a613dbaaa74e18f7d0502f2fc743fe.js
+++ build/precache-manifest.e7a613dbaaa74e18f7d0502f2fc743fe.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "a0b4ad4db3078ad3f61a",
-    "url": "/arahman1983/expensefy-app/static/js/runtime~main.a0b4ad4d.js"
+    revision: "a0b4ad4db3078ad3f61a",
+    url: "static/js/runtime~main.a0b4ad4d.js"
   },
   {
-    "revision": "dd7cabcf5fe606480054",
-    "url": "/arahman1983/expensefy-app/static/js/main.dd7cabcf.chunk.js"
+    revision: "dd7cabcf5fe606480054",
+    url: "static/js/main.dd7cabcf.chunk.js"
   },
   {
-    "revision": "23c33caf563327a5258c",
-    "url": "/arahman1983/expensefy-app/static/js/1.23c33caf.chunk.js"
+    revision: "23c33caf563327a5258c",
+    url: "static/js/1.23c33caf.chunk.js"
   },
   {
-    "revision": "dd7cabcf5fe606480054",
-    "url": "/arahman1983/expensefy-app/static/css/main.c9c5a9ed.chunk.css"
+    revision: "dd7cabcf5fe606480054",
+    url: "static/css/main.c9c5a9ed.chunk.css"
   },
   {
-    "revision": "23c33caf563327a5258c",
-    "url": "/arahman1983/expensefy-app/static/css/1.5fe73066.chunk.css"
+    revision: "23c33caf563327a5258c",
+    url: "static/css/1.5fe73066.chunk.css"
   },
   {
-    "revision": "6278e271cff65225f14d3f2a2c4bd667",
-    "url": "/arahman1983/expensefy-app/index.html"
+    revision: "6278e271cff65225f14d3f2a2c4bd667",
+    url: "index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
-
 importScripts(
-  "/arahman1983/expensefy-app/precache-manifest.e7a613dbaaa74e18f7d0502f2fc743fe.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js"
 );
+
+importScripts("precache-manifest.e7a613dbaaa74e18f7d0502f2fc743fe.js");
 
 workbox.clientsClaim();
 
@@ -28,7 +28,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/arahman1983/expensefy-app/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute("index.html", {
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
 });
